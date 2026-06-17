046b888fc514f0e97d24e3a9263528c1b7217d79
Rickson Zoren Pineda <rzvpineda@gmail.com>
2026-06-17 11:55:46 +0800

    DLL creation

--- HelloWorld/obj/Debug/net10.0/TCLASS ELECTIVE 1.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/TCLASS ELECTIVE 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCLASS ELECTIVE 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6077dbaae333761ef4301ce42d87969f38544554")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCLASS ELECTIVE 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCLASS ELECTIVE 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
